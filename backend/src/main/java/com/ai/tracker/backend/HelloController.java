4f34f9bc99803e67bd07f1eb44bbce82d8dfd1ba
AnishJaiswal4444 <anish.jaiswal@dynapeak.tech>
2025-07-18 00:01:57 +0530

    Day 2: Setup Spring Boot project and created Hello API

---
+++ backend/src/main/java/com/ai/tracker/backend/HelloController.java
@@ -0,0 +1,12 @@
+package com.ai.tracker.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "👋 Hello from AI Learning Tracker App!";
+    }
+}
